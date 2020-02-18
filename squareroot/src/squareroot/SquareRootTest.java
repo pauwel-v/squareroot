ce08a58797f4592d0355c001f16ea35666e68da0
pauwe <pauwe@10.46.156.176>
2020-02-18 17:38:47 +0100

    Initial commit

--- squareroot/src/squareroot/SquareRootTest.java
+++ squareroot/src/squareroot/SquareRootTest.java
@@ -8,7 +8,7 @@ class SquareRootTest {
 
 	@Test
 	void test() {
-		assert SquareRoot.squareRoot(0) == 0;
+		assert SquareRoot.squareRoot(0) != 0;
 		assert SquareRoot.squareRoot(9) == 3;
 		assert SquareRoot.squareRoot(10) == 3;
 		assert SquareRoot.squareRoot(16) == 4;
