51c865cd81509884b6f8795188cbcd4bbd8f213b
pauwe <pauwe@10.46.156.176>
2020-02-18 17:07:59 +0100

    Initial commit

---
+++ squareroot/src/squareroot/SquareRoot.java
@@ -0,0 +1,19 @@
+package squareroot;
+
+public class SquareRoot {
+	/**
+	 * Returns the square root, rounded down, of the given number.
+	 * 
+	 * @pre The given number is nonnegative.
+	 * 		| 0 <= x
+	 * @post The square of the result is not greater than the given number.
+	 * 		| result * result <= x
+	 * @post The result is the greatest number whose square is not greater than the given number.
+	 */
+	public static int squareRoot(int x) {
+		int result = 0;
+		while (result*result <= x)
+			result++;
+		return result - 1;
+	}
+}
